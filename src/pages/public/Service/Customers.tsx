7eeb663435ebf492527c6bf3eecb7b5d95e5a8bc
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2025-07-06 14:37:28 +0530

    feat: Remove Pricing component from Customers page

--- src/pages/public/Service/Customers.tsx
+++ src/pages/public/Service/Customers.tsx
@@ -1,5 +1,4 @@
 import { Contact } from "@/components/Page/Public/Index/Contact";
-import { Pricing } from "@/components/Page/Public/Index/Pricing";
 import BenefitsSection from "@/components/Page/Public/Service/Customers/BenefitsSection";
 import FaqSection from "@/components/Page/Public/Service/Customers/Faqsection";
 import HeroSection from "@/components/Page/Public/Service/Customers/HeroSection";
@@ -11,7 +10,6 @@ export default function Customers(): JSX.Element {
       <HeroSection />
       <WhyUseSection />
       <BenefitsSection />
-      <Pricing />
       <FaqSection />
       <Contact />
     </div>
